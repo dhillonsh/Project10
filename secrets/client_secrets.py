aeb95157ef5f8c0e554a8eb514dd90ef270d7512
dhillonsh <dhillonsh@gmail.com>
2016-12-09 00:30:33 -0800

    Update client_secrets.py

--- secrets/client_secrets.py
+++ secrets/client_secrets.py
@@ -1,12 +1,9 @@
-# MongoDB client secrets
-#
-
-author = "Harpreet Dhillon"                               # As it appears in Canvas
-repo = "https://github.com/dhillonsh/Project10.git"  # Replace with yours
+author = "Harpreet Dhillon"
+repo = "https://github.com/dhillonsh/Project10.git"
 
 # The database we will use within the MongoDB instance
 # for the Memos of Mongo application 
 db = "meetingList"
 # The MongoDB user who will have access to read and write this database
 db_user = "meeting_user"
-db_user_pw = "meeting_pass"  # Can you identify the poem? 
+db_user_pw = "meeting_pass" 
